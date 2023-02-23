71dd6c95d7bd19ba31ef3c650b3182bbcb48edbf
pppig236 <23kongw@brrice.edu>
2023-02-23 11:25:44 -0500

    rename to pppig

--- env.sh
+++ env.sh
@@ -7,10 +7,10 @@ export ROCM_PATCH_VERSION=0
 export ROCM_LIBPATCH_VERSION=50300
 export CPACK_DEBIAN_PACKAGE_RELEASE=63~20.04
 export ROCM_PKGTYPE=DEB
-export ROCM_GIT_DIR=/home/work/ROCm
-export ROCM_BUILD_DIR=/home/work/rocm-build/build
-export ROCM_PATCH_DIR=/home/work/rocm-build/patch
-export AMDGPU_TARGETS="gfx803"
-export CMAKE_DIR=/home/work/local/cmake-3.18.6-Linux-x86_64
+export ROCM_GIT_DIR=/home/pppig/ROCm
+export ROCM_BUILD_DIR=/home/pppig/rocm-build/build
+export ROCM_PATCH_DIR=/home/pppig/rocm-build/patch
+export AMDGPU_TARGETS="gfx1030"
+export CMAKE_DIR=/home/pppig/Downloads/cmake-3.18.6-Linux-x86_64/
 export PATH=$ROCM_INSTALL_DIR/bin:$ROCM_INSTALL_DIR/llvm/bin:$ROCM_INSTALL_DIR/hip/bin:$CMAKE_DIR/bin:$PATH
 
