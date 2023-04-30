7c14a73a7e0d5a5b40976e38f5cc55ea1e594148
pppig236 <23kongw@brrice.edu>
2023-04-30 03:46:14 -0400

    lib64 -> lib

--- check/run-rocblas4.sh
+++ check/run-rocblas4.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 rm build/test_rocblas4
 
-hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L/opt/rocm/lib64 src/test_rocblas4.cpp -o build/test_rocblas4
+hipcc -D__HIP_PLATFORM_HCC__ -lrocblas -L/opt/rocm/lib src/test_rocblas4.cpp -o build/test_rocblas4
 
 # ROCBLAS_LAYER=0xf TENSILE_DB=0xffff ./build/test_rocblas3
 ./build/test_rocblas4
